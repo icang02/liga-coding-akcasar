889cfef7e68509314a97d1a8f5e93a9f37d8e93b
icang02 <ilmifaizan1112@gmail.com>
2022-08-20 17:19:41 +0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+let navbar = document.querySelector('header');
+let arrowTop = document.querySelector('.arrow-top');
+arrowTop.style.display = "none";
+
 window.addEventListener('scroll', function() {
-  let navbar = document.querySelector('header');
   
   if (window.scrollY > 0) {
-    navbar.style.transform = "translateY(-60px)"
+    navbar.style.transform = "translateY(-60px)";
+    arrowTop.style.display = "block";
   } else {
-    navbar.style.transform = "translateY(0px)"
+    navbar.style.transform = "translateY(0px)";
+    arrowTop.style.display = "none";
   }
 });
+
+const backTop = () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
